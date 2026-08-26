8c331086ef786329dd52ee535f946c40478147c1
TocnoWamGovoriyYaNeHacer201145 <hacergames861@gmail.com>
2026-08-26 19:43:15 +0300

    Minor update
    Renamed "r" to "running";
    Added "type" word (Example of usage: "10 type");
    Added "exec" word;
    Added "init_mem" word (Example of usage: "100 init_mem");
    "push" function is now a macro;

--- main.cpp
+++ main.cpp
@@ -11,8 +11,7 @@ using StackValue = std::variant<int, std::string>;
 std::vector<StackValue> stack;
 std::vector<StackValue> ret_stack;
 
-// Not gonna lie, I will do something to this thing...
-StackValue memory[1000];
+std::vector<StackValue> memory;
 
 std::unordered_map<std::string, std::function<void()>> builtins;
 std::unordered_map<std::string, std::pair<int, std::function<void()>>> compile_words;
@@ -20,50 +19,48 @@ std::unordered_map<std::string, std::pair<int, std::function<void()>>> compile_w
 std::unordered_map<std::string, std::string> functions;
 std::string body;
 
-int r = 1;
+int running = 1;
 int compiling = 0;
 
 #define to_int(obj) std::get<int>(obj)
 #define to_str(obj) std::get<std::string>(obj).c_str()
 
-inline void push(StackValue val) {
-    stack.push_back(val);
-}
-
+#define push(val) stack.push_back(val)
 inline StackValue pop() {
     StackValue obj = stack.back(); 
     stack.pop_back();
     return obj;
 }
 
-// Functions prototypes!!!
+// Functions prototypes
 void run(std::string);
 void run_file(std::string);
 
 // Inlined, because it is used only once
 inline void init_builtins() {
-    builtins["+"] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a + b);};
-    builtins["-"] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a - b);};
-    builtins["*"] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a * b);};
-    builtins["/"] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a / b);};
-    builtins["%"] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a % b);};
+    builtins["+"] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a + b); };
+    builtins["-"] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a - b); };
+    builtins["*"] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a * b); };
+    builtins["/"] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a / b); };
+    builtins["%"] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a % b); };
 
-    builtins["<"] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a < b);};
-    builtins[">"] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a > b);};
-    builtins["=="] = []() {StackValue b = pop(); StackValue a = pop(); push(a == b);};
-    builtins["!="] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a != b);};
-    builtins[">="] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a >= b);};
-    builtins["<="] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a <= b);};
+    builtins["<"] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a < b); };
+    builtins[">"] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a > b); };
+    builtins["=="] = []() { StackValue b = pop(); StackValue a = pop(); push(a == b); };
+    builtins["!="] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a != b); };
+    builtins[">="] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a >= b);};
+    builtins["<="] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a <= b); };
 
-    builtins["and"] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a & b);};
-    builtins["or"] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a | b);};
-    builtins["xor"] = []() {int b = to_int(pop()); int a = to_int(pop()); push(a ^ b);};
-    builtins["not"] = []() {int a = to_int(pop()); push(~a);};
+    builtins["and"] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a & b); };
+    builtins["or"] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a | b); };
+    builtins["xor"] = []() { int b = to_int(pop()); int a = to_int(pop()); push(a ^ b); };
+    builtins["not"] = []() { int a = to_int(pop()); push(~a); };
 
-    builtins["import"] = []() {run_file(to_str(pop()));};
+    builtins["import"] = []() { run_file(to_str(pop()));} ;
 
-    builtins["!"] = []() {int addr = to_int(pop()); StackValue value = pop(); memory[addr] = value;};
-    builtins["@"] = []() {push(memory[to_int(pop())]);};
+    builtins["init_mem"] = []() { memory.reserve(to_int(pop())); };
+    builtins["!"] = []() { int addr = to_int(pop()); StackValue value = pop(); memory[addr] = value; };
+    builtins["@"] = []() { push(memory[to_int(pop())]);} ;
 
     builtins["."] = []() {
         StackValue value = pop();
@@ -73,35 +70,44 @@ inline void init_builtins() {
             std::cout << std::get<std::string>(value) << " ";
         }
     };
-    builtins["emit"] = []() {putchar_unlocked(to_int(pop()));};
-    builtins[","] = []() {push(getchar_unlocked());};
+    builtins["emit"] = []() { putchar_unlocked(to_int(pop())); };
+    builtins[","] = []() { push(getchar_unlocked()); };
 
     builtins["nl"] = []() {std::cout << std::endl;};
 
-    builtins["print"] = []() {builtins["."](); builtins["nl"]();};
-    builtins["input"] = []() {std::string line; std::getline(std::cin, line); push(line);};
+    builtins["print"] = []() { builtins["."](); builtins["nl"](); };
+    builtins["input"] = []() { std::string line; std::getline(std::cin, line); push(line); };
 
-    builtins["dup"] = []() {push(stack.back());};
-    builtins["swap"] = []() {StackValue a = pop(); StackValue b = pop(); push(a); push(b);};
-    builtins["over"] = []() {push(stack[stack.size() - 2]);};
-    builtins["drop"] = []() {pop();};
-    builtins["depth"] = []() {push(static_cast<int>(stack.size()));};
-    builtins["clear"] = []() {stack.clear();};
+    builtins["dup"] = []() { push(stack.back()); };
+    builtins["swap"] = []() { StackValue a = pop(); StackValue b = pop(); push(a); push(b); };
+    builtins["over"] = []() { push(stack[stack.size() - 2]); };
+    builtins["drop"] = []() { pop(); };
+    builtins["depth"] = []() { push(static_cast<int>(stack.size())); };
+    builtins["clear"] = []() { stack.clear(); };
 
-    builtins["stoi"] = []() {push(std::stoi(std::get<std::string>(pop())));};
-    // builtins["type"] = []() {push(typeid(pop()).name());};
-
-    builtins[">r"] = []() {ret_stack.push_back(pop());};
-    builtins["r>"] = []() {StackValue obj = ret_stack.back(); ret_stack.pop_back(); push(obj);};
-    builtins["r@"] = []() {push(ret_stack.back());};
-
-    builtins["{"] = []() {compiling += 1;};
-    builtins["if"] = []() {compiling += 2;};
-    builtins["\""] = []() {compiling += 3;};
+    builtins["stoi"] = []() { push(std::stoi(std::get<std::string>(pop()))); };
+    builtins["type"] = []() {
+        StackValue value = pop();
+        if (std::holds_alternative<int>(value)) {
+            push("int");
+        } else {
+            push("str");
+        }
+    };
     
-    builtins["concat"] = []() {std::string b = to_str(pop()); push(to_str(pop()) + b);};
+    builtins["exec"] = []() { builtins[to_str(pop())](); };
 
-    builtins["bye"] = []() {r = 0;};
+    builtins[">r"] = []() { ret_stack.push_back(pop()); };
+    builtins["r>"] = []() { StackValue obj = ret_stack.back(); ret_stack.pop_back(); push(obj); };
+    builtins["r@"] = []() { push(ret_stack.back()); };
+
+    builtins["{"] = []() { compiling += 1; };
+    builtins["if"] = []() { compiling += 2; };
+    builtins["\""] = []() { compiling += 3; };
+    
+    builtins["concat"] = []() { std::string b = to_str(pop()); push(to_str(pop()) + b); };
+
+    builtins["bye"] = []() { running = 0; };
 
     compile_words["}"] = {1, 
         []() {
@@ -196,11 +202,11 @@ int main(int argc, char** argv) {
         }
     } else {
         std::cout << "Usage: <compiler binary> [files]" << std::endl;
-        r = 0;
+        running = 0;
     }
 
     std::string line;
-    while (r && std::getline(std::cin, line)) {
+    while (running && std::getline(std::cin, line)) {
         run(line);
     }
     return 0;
